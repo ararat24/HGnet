5f5875125524e7f8ae0a2c824741f1a0e10eb731
Ararat <yesayan.ararat0@gmail.com>
2022-01-24 16:49:25 +0400

    Added all ejs files

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ app.get('/', (req, res)=>{
     res.render('index')
 })
 
+app.get('/admin', (req, res)=>{
+    res.render('admin/admin')
+})
+
+app.get('/admin/tarifs', (req, res)=>{
+    res.render('admin/tarifs')
+})
+
+app.get('/admin/accounts', (req, res) =>{
+    res.render('admin/accounts')
+})
 app.listen(3000)
